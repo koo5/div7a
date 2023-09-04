b931da2b6508ac53d08820855b11d1f47ae37373
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-09-04 14:02:09 +0200

    ugh

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -1,11 +1,15 @@
 /// <reference types="cypress" />
 
+/*
+python3 -m pip install -U "watchdog[watchmedo]"
+watchmedo auto-restart -q -d data -- fish -c 'cd data; echo -e "\n\n\n"; tail -n 55555555 -f (ls -t . | head -1)'
+*/
+
 describe('example to-do app', () => {
   beforeEach(() => {
 	cy.visit('https://www.ato.gov.au/Calculators-and-tools/Host/?anchor=DIV7A#DIV7A/questions')
-	//cy.clock()
-	cy.viewport(1000, 2080)
-	
+	cy.viewport(1000, 1080)
+	idx = 0;
   })
 
   it('generate_testcase', () => {
@@ -20,12 +24,12 @@ describe('example to-do app', () => {
 			cy.get('[id=ddl-incomeYearOfLoan]').select(incomeYearOfLoan_text);
 			cy.get('[id=vrb-loanType-span-1]').click();
 			
-			let tc = {
-				incomeYearOfLoan:incomeYearOfLoan_number
+			let tc0 = {
+				incomeYearOfLoan:incomeYearOfLoan_number,
 			};
 
-
-			for (var term = 1; term < 8; term++)
+			var termsArr = Array.from({length:7},(v,k)=>k+1)
+			cy.wrap(termsArr).each((term) => 
 			{
 				cy.get('[id=textfullTermOfAmalgamatedLoan]').type('{selectall}{backspace}' + term,{delay: 0});
 
@@ -47,9 +51,9 @@ describe('example to-do app', () => {
 						cy.get('[id=textamalgamatedLoanNotPaidByEOIY]').type('{selectall}{backspace}' + amalgamatedLoanNotPaidByEOIY,{delay: 0});
 						cy.get('[id=vrb-calculateAmountOfTheAmalgamatedLoan-span-0]').click();
 
-						var tc = {
-							...tc,
+						let tc = {
 							repayments:[],
+							...tc0,
 							fullTermOfAmalgamatedLoan: term,
 							incomeYearOfEnquiring:incomeYearOfEnquiring_number,
 							amalgamatedLoanNotPaidByEOIY:amalgamatedLoanNotPaidByEOIY,
@@ -61,6 +65,7 @@ describe('example to-do app', () => {
 						let sparsity = Math.floor(Math.random() * 1000);
 						let start = new Date(min);
 						start.setDate(start.getDate() + Math.floor(Math.random() * sparsity));
+										
 						for (var d = new Date(min); d <= max && remaining > 0; d.setDate(d.getDate() + 1 + Math.floor(Math.random() * sparsity)))
 						{
 							let rd = d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear();
@@ -70,24 +75,26 @@ describe('example to-do app', () => {
 							remaining -= ra;
 							if (remaining < 0)
 								ra += remaining;
-
 							tc.repayments.push({rd,ra})
-							
-							cy.get('[id="btn_repayment_add"]').click();
-							cy.get('[id="textdateOfLoanRepaymentadd"]').type('{selectall}{backspace}' + rd,{delay: 0});
-							cy.get('[id="textamountOfRepaymentadd"]').type('{selectall}{backspace}' + ra,{delay: 0});
-							cy.get('[id="btn_repayment_save"]').click()
-							f1(tc,incomeYearOfEnquiring_number,incomeYearOfLoan_number);
 						}
 
-						for (var i = 0; i < tc.repayments.length; i++)
+						cy.wrap(tc.repayments).each((r) =>
+						{		
+							cy.get('[id="btn_repayment_add"]').click();
+							cy.get('[id="textdateOfLoanRepaymentadd"]').type('{selectall}{backspace}' + r.rd,{delay: 0});
+							cy.get('[id="textamountOfRepaymentadd"]').type('{selectall}{backspace}' + r.ra,{delay: 0});
+							cy.get('[id="btn_repayment_save"]').click()
+							f1(tc,incomeYearOfEnquiring_number,incomeYearOfLoan_number);
+						});
+
+						cy.wrap(tc.repayments).each((_) => 
 						{
 							cy.get('[id^="toggle_collapseMethod_repayment"]').first().click()
 							cy.get('[id="btn_repayment_delete"]').first().click()
-						}						
+						});						
 					}	
 				});
-			}
+			});
 		}
 	});
   })
@@ -107,10 +114,15 @@ function f1(tc,incomeYearOfEnquiring_number,incomeYearOfLoan_number)
 		let min = new Date(incomeYearOfLoan_number,  7-1, 1);
 		let max = new Date(incomeYearOfLoan_number+1,6-1, 30);
 
+		var dates = [];
 		for (var d = new Date(min); d <= max; d.setDate(d.getDate() + 100))
+			dates.push(new Date(d));
+		
+		cy.wrap(dates).each((d) =>
 		{
 			lodgement_f(tc, d)
-		}
+		});
+		
 		lodgement_f(tc, max);
 	}
 }
@@ -126,34 +138,35 @@ function lodgement_f(tc, d)
 	})
 }
 
+var idx = 0;
+
 function f2(tc0)
 {
-	//cy.wait(100)
-	//cy.scrollTo('bottom', {ensureScrollable: false})
-	//cy.wait(10000)
-	//cy.contains('Show result').should('exist').should('be.visible').click();
+	cy.contains('Show result').first().trigger('click').then(() => {
 	
-	cy.contains('Show result').first().trigger('click')
-	//cy.wait(10000)
+		let outputs = {}
+		let tc = {
+			inputs: tc0,
+			outputs,
+			id: new Date().toISOString() + Math.random(),
+			idx: idx++,
+		}
+
+		cy.wrap(db(outputs,'enquiryRateFormatted'), {log:true})
+		cy.wrap(db(outputs,'enquiryYearEndMinusOneDisplay'), {log:true})
+		cy.wrap(db(outputs,'amalgamatedLoanNotPaidByEOIYFormatted'), {log:true})
+		cy.wrap(db(outputs,'openingBalancePayDays'), {log:true})
+		cy.wrap(db(outputs,'openingBalanceInterestFormatted'), {log:true})
+		cy.wrap(db(outputs,'totalAmountOfRepaymentFormatted'), {log:true})
+		cy.wrap(db(outputs,'totalInterestFormatted'), {log:true})
+		cy.wrap(db(outputs,'principalFormatted'), {log:true})
+		cy.wrap(db(outputs,'enquiryYearEndDisplay'), {log:true})
+		cy.wrap(db(outputs,'minimumYearRepaymentFormatted'), {log:true})
+		
+		cy.get('.alert-attention').then(($el) => {outputs['alert'] = $el[0].innerText})
 	
-	let tc = {
-		...tc0,
-		id: new Date().toISOString() + Math.random()
-	}
-	cy.get('.alert-attention').then(($el) => {tc['alert'] = $el[0].innerText});
-	db(tc,'enquiryRateFormatted')
-   	db(tc,'enquiryYearEndMinusOneDisplay')
-   	db(tc,'amalgamatedLoanNotPaidByEOIYFormatted')
-   	db(tc,'openingBalancePayDays')
-   	db(tc,'openingBalanceInterestFormatted')
-   	db(tc,'totalAmountOfRepaymentFormatted')
-   	db(tc,'totalInterestFormatted')
-   	db(tc,'principalFormatted')
-   	db(tc,'enquiryYearEndDisplay')
-   	db(tc,'minimumYearRepaymentFormatted')
-        	
-	cy.writeFile('data/' + tc.id + '.json', tc)
-	cy.writeFile('data/last.json', tc);
+		cy.writeFile('data/' + tc.idx + '_' + tc.id + '.json', tc)
+	})
 }
 
 function db(tc, k)
